bff67445ad3a5c53e05d9b1dd9be602ff5011808
briantam23 <briantam23@gmail.com>
2018-09-15 10:05:12 -0400

    first commit

---
+++ db/index.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme-products-react-redux', {
+    logging: false
+});
+
+const Product = conn.define('product', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    rating: Sequelize.INTEGER
+})
+
+const syncAndSeed = () => {
+    conn.sync({ force: true })
+        .then(() => { Promise.all([
+            Product.create({ name: 'Laptop', rating: 4 }),
+            Product.create({ name: 'headphones', rating: 3 }),
+            Product.create({ name: 'backpack', rating: 4 })
+        ])})
+}
+
+module.exports = {
+    syncAndSeed,
+    models: {
+        Product
+    }
+}
---
+++ src/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => (
+    <div>
+        <h1>Acme-Products-React-Redux</h1>
+        <ul>
+            <li>Products (X)</li>
+            <li>Top Rated</li>
+        </ul>
+    </div>
+)
